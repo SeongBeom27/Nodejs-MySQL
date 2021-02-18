a29f814e790ea740bfdb2dc2961c0259de2e77f4
SeongBeom27 <lgh121546@naver.com>
2021-02-18 14:39:02 +0900

    Node SQL 9. 저자 목록 보기 구현

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var template = require('./lib/template.js');
 var sanitizeHtml = require('sanitize-html');
 var db = require('./lib/db');
 var topic = require('./lib/topic');
+var author = require('./lib/author');
 
 var app = http.createServer(function(request, response) {
     var _url = request.url;
@@ -27,6 +28,8 @@ var app = http.createServer(function(request, response) {
         topic.update_process(request, response);
     } else if (pathname === '/delete_process') {
         topic.delete_process(request, response);
+    } else if (pathname === '/author') {
+        author.home(response);
     } else {
         response.writeHead(404);
         response.end('Not found');
