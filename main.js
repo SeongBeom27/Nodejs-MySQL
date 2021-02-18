ff1645415a8cc01b43e54246b29d74c0f75a59a3
SeongBeom27 <lgh121546@naver.com>
2021-02-18 16:31:34 +0900

    Node SQL 10. 저자 생성 수정 삭제 기능 구현

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
+    /**
+     *  아래와 같이 경로로 보내주는 것을 "라우팅" 이라고 한다.
+     *  여러 프레임워크에서는 아래와 같이 조건문이 아닌 좀 더 간결하게 사용할 수 있도록  제공해준다.
+     */
     if (pathname === '/') {
         if (queryData.id === undefined) {
             // home
@@ -30,6 +34,14 @@ var app = http.createServer(function(request, response) {
         topic.delete_process(request, response);
     } else if (pathname === '/author') {
         author.home(response);
+    } else if (pathname === '/author/create_process') {
+        author.create_process(request, response);
+    } else if (pathname === '/author/update') {
+        author.update(request, response);
+    } else if (pathname === '/author/update_process') {
+        author.update_process(request, response);
+    } else if (pathname === '/author/delete_process') {
+        author.delete_process(request, response);
     } else {
         response.writeHead(404);
         response.end('Not found');
